bf7b928072bfe09ef03999e9fd6c9ef3707dd684
Roland Bewick <roland.bewick@gmail.com>
2022-10-12 22:38:01 +0700

    Add nextui

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { NextUIProvider } from "@nextui-org/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <NextUIProvider>
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </NextUIProvider>
   );
 }
 
