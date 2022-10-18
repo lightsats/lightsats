4be67d672de8422ffbe0b32e94c30e66d754b785
Roland Bewick <roland.bewick@gmail.com>
2022-10-18 15:05:32 +0700

    Add exchange rates endpoint

---
+++ app/pages/api/exchange/rates.ts
@@ -0,0 +1,82 @@
+import { hoursToSeconds } from "date-fns";
+import { NextApiRequest, NextApiResponse } from "next";
+import NodeCache from "node-cache";
+import { ExchangeRates } from "../../../types/ExchangeRates";
+
+const exchangeRatesCacheKey = "exchangeRates";
+const exchangeRatesCache = new NodeCache();
+
+// type CachedExchangeRates
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ExchangeRates>
+) {
+  let exchangeRates = exchangeRatesCache.get<ExchangeRates>(
+    exchangeRatesCacheKey
+  );
+  if (!exchangeRates) {
+    const btcUsdPrice = await getBtcUsdPrice();
+    console.log("BTCUSD", btcUsdPrice);
+    const fiatExchangeRates = await getFiatExchangeRates();
+    console.log("fiatExchangeRates", fiatExchangeRates);
+
+    exchangeRates = {};
+    for (const fiatExchangeRate of Object.entries(fiatExchangeRates)) {
+      exchangeRates[fiatExchangeRate[0]] = btcUsdPrice * fiatExchangeRate[1];
+    }
+    console.log("exchangeRates", fiatExchangeRates);
+
+    exchangeRatesCache.set(
+      exchangeRatesCacheKey,
+      exchangeRates,
+      hoursToSeconds(24)
+    );
+  } else {
+    console.log("Exchange rates are cached");
+  }
+
+  return res.json(exchangeRates);
+}
+
+async function getBtcUsdPrice() {
+  const tickerResponse = await fetch(
+    `https://api.kraken.com/0/public/Ticker?pair=XBTUSD`,
+    {
+      method: "GET",
+    }
+  );
+
+  if (!tickerResponse.ok) {
+    throw new Error("Failed to get kraken ticker data");
+  }
+
+  const responseBody = (await tickerResponse.json()) as {
+    result: {
+      [pair: string]: { p: string[] };
+    };
+  };
+
+  return parseFloat(Object.values(responseBody.result)[0].p[0]);
+}
+
+async function getFiatExchangeRates() {
+  const exchangeRatesResponse = await fetch(
+    `https://open.er-api.com/v6/latest/USD`,
+    {
+      method: "GET",
+    }
+  );
+
+  if (!exchangeRatesResponse.ok) {
+    throw new Error("Failed to get exchange rates data");
+  }
+
+  const exhangeRates = (
+    (await exchangeRatesResponse.json()) as {
+      rates: { [currency: string]: number };
+    }
+  ).rates;
+
+  return exhangeRates;
+}
---
+++ app/types/ExchangeRates.ts
@@ -0,0 +1,4 @@
+/**
+ * Price of 1 Bitcoin in fiat currencies
+ */
+export type ExchangeRates = { [currency: string]: number };
