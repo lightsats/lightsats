b55b153336e114ea0b3fcfda11e4869de09401c6
Roland Bewick <roland.bewick@gmail.com>
2022-10-19 13:50:34 +0700

    Convert new tip form to use react-hook-form

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -3,6 +3,7 @@ import { TipStatus } from "@prisma/client";
 export const appName = "lightsats";
 export const SATS_TO_BTC = 100000000;
 export const DEFAULT_FIAT_CURRENCY = "USD";
+export const MIN_TIP_SATS = 1;
 
 export const refundableTipStatuses: TipStatus[] = [
   "UNCLAIMED",
--- app/pages/tips/new.tsx
+++ app/pages/tips/new.tsx
@@ -11,42 +11,66 @@ import { Tip } from "@prisma/client";
 import { BackButton } from "components/BackButton";
 import { FiatPrice } from "components/FiatPrice";
 import { SatsPrice } from "components/SatsPrice";
+import { MIN_TIP_SATS } from "lib/constants";
 import { Routes } from "lib/Routes";
 import { defaultFetcher } from "lib/swr";
 import { getFiatAmount, getSatsAmount } from "lib/utils";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import React from "react";
+import { Controller, useForm } from "react-hook-form";
 import useSWR from "swr";
 import { CreateTipRequest } from "types/CreateTipRequest";
 import { ExchangeRates } from "types/ExchangeRates";
 
+type NewTipFormData = {
+  amount: number;
+  currency: string;
+};
+
+const formStyle: React.CSSProperties = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+};
+
 const NewTip: NextPage = () => {
   // TODO: use a proper form
   const router = useRouter();
-  const [amount, setAmount] = React.useState(1);
   const [isSubmitting, setSubmitting] = React.useState(false);
   const [inputMethod, setInputMethod] = React.useState<"fiat" | "sats">("fiat");
-
-  const [currency, setCurrency] = React.useState("USD");
 
   const { data: exchangeRates } = useSWR<ExchangeRates>(
     `/api/exchange/rates`,
     defaultFetcher
   );
 
-  const currentExchangeRate = exchangeRates?.[currency];
+  const { control, handleSubmit, watch, setValue } = useForm<NewTipFormData>({
+    defaultValues: {
+      amount: 1,
+      currency: "USD",
+    },
+  });
+
+  const watchedAmount = watch("amount");
+  const watchedCurrency = watch("currency");
+
+  const currentExchangeRate = exchangeRates?.[watchedCurrency];
 
   const toggleInputMethod = React.useCallback(() => {
     if (currentExchangeRate) {
       setInputMethod(inputMethod === "fiat" ? "sats" : "fiat");
-      setAmount(
+      setValue(
+        "amount",
         inputMethod === "fiat"
-          ? getSatsAmount(amount, currentExchangeRate)
-          : Math.round(getFiatAmount(amount, currentExchangeRate) * 100) / 100
+          ? getSatsAmount(watchedAmount, currentExchangeRate)
+          : Math.round(
+              getFiatAmount(watchedAmount, currentExchangeRate) * 100
+            ) / 100
       );
     }
-  }, [amount, currentExchangeRate, inputMethod]);
+  }, [watchedAmount, currentExchangeRate, inputMethod, setValue]);
 
   const exchangeRateKeys = React.useMemo(
     () => (exchangeRates ? Object.keys(exchangeRates) : undefined),
@@ -54,104 +78,126 @@ const NewTip: NextPage = () => {
   );
 
   const setDropdownSelectedCurrency = React.useCallback(
-    (keys: unknown) => setCurrency(Array.from(keys as Iterable<string>)[0]),
-    []
+    (keys: unknown) =>
+      setValue("currency", Array.from(keys as Iterable<string>)[0]),
+    [setValue]
   );
 
   const selectedCurrencies = React.useMemo(
-    () => new Set([currency]),
-    [currency]
+    () => new Set([watchedCurrency]),
+    [watchedCurrency]
   );
 
-  const submitForm = React.useCallback(() => {
-    if (isSubmitting) {
-      throw new Error("Already submitting");
-    }
-    if (amount <= 0) {
-      throw new Error("No amount provided");
-    }
-    setSubmitting(true);
-
-    (async () => {
-      try {
-        const createTipRequest: CreateTipRequest = {
-          amount,
-          currency,
-        };
-        const result = await fetch("/api/tipper/tips", {
-          method: "POST",
-          body: JSON.stringify(createTipRequest),
-          headers: { "Content-Type": "application/json" },
-        });
-        if (result.ok) {
-          const tip = (await result.json()) as Tip;
-          // TODO: save the tip in SWR's cache so it is immediately available
-          router.push(`${Routes.tips}/${tip.id}`);
-        } else {
-          alert("Failed to create tip: " + result.statusText);
-        }
-      } catch (error) {
-        console.error(error);
-        alert("Tip creation failed. Please try again.");
+  const onSubmit = React.useCallback(
+    (data: NewTipFormData) => {
+      if (!currentExchangeRate) {
+        throw new Error("Exchange rates not loaded");
       }
-      setSubmitting(false);
-    })();
-  }, [amount, currency, isSubmitting, router]);
+      if (isSubmitting) {
+        throw new Error("Already submitting");
+      }
+      const satsAmount =
+        inputMethod === "fiat"
+          ? getSatsAmount(data.amount, currentExchangeRate)
+          : data.amount;
+      if (satsAmount < MIN_TIP_SATS) {
+        throw new Error("Tip amount is too small");
+      }
+      setSubmitting(true);
+
+      (async () => {
+        try {
+          const createTipRequest: CreateTipRequest = {
+            amount: satsAmount,
+            currency: data.currency,
+          };
+          const result = await fetch("/api/tipper/tips", {
+            method: "POST",
+            body: JSON.stringify(createTipRequest),
+            headers: { "Content-Type": "application/json" },
+          });
+          if (result.ok) {
+            const tip = (await result.json()) as Tip;
+            // TODO: save the tip in SWR's cache so it is immediately available
+            router.push(`${Routes.tips}/${tip.id}`);
+          } else {
+            alert("Failed to create tip: " + result.statusText);
+          }
+        } catch (error) {
+          console.error(error);
+          alert("Tip creation failed. Please try again.");
+        }
+        setSubmitting(false);
+      })();
+    },
+    [currentExchangeRate, inputMethod, isSubmitting, router]
+  );
 
   return (
     <>
-      <Row justify="center" align="center">
-        <Link onClick={toggleInputMethod}>{inputMethod}</Link>
-        <Spacer x={0.5} />
-        <Input
-          width="100px"
-          type="number"
-          inputMode="decimal"
-          value={amount.toString()}
-          onChange={(event) => setAmount(parseFloat(event.target.value || "0"))}
-        />
-        <Spacer x={0.5} />
-        {exchangeRateKeys && (
-          <Dropdown>
-            <Dropdown.Button flat>
-              {inputMethod === "sats" ? (
-                <FiatPrice
-                  currency={currency}
-                  exchangeRate={exchangeRates?.[currency]}
-                  sats={amount}
-                />
-              ) : (
-                currency
-              )}
-            </Dropdown.Button>
-            <Dropdown.Menu
-              aria-label="Dynamic Actions"
-              selectionMode="single"
-              selectedKeys={selectedCurrencies}
-              onSelectionChange={setDropdownSelectedCurrency}
-            >
-              {exchangeRateKeys.map((key) => (
-                <Dropdown.Item key={key}>{key}</Dropdown.Item>
-              ))}
-            </Dropdown.Menu>
-          </Dropdown>
+      <form onSubmit={handleSubmit(onSubmit)} style={formStyle}>
+        <Row justify="center" align="center">
+          <Link onClick={toggleInputMethod}>{inputMethod}</Link>
+          <Spacer x={0.5} />
+          <Controller
+            name="amount"
+            control={control}
+            render={({ field }) => (
+              <Input
+                {...field}
+                width="100px"
+                type="number"
+                inputMode="decimal"
+              />
+            )}
+          />
+
+          <Spacer x={0.5} />
+          {exchangeRateKeys && (
+            <Dropdown>
+              <Dropdown.Button flat>
+                {inputMethod === "sats" ? (
+                  <FiatPrice
+                    currency={watchedCurrency}
+                    exchangeRate={exchangeRates?.[watchedCurrency]}
+                    sats={watchedAmount}
+                  />
+                ) : (
+                  watchedCurrency
+                )}
+              </Dropdown.Button>
+              <Dropdown.Menu
+                aria-label="Dynamic Actions"
+                selectionMode="single"
+                selectedKeys={selectedCurrencies}
+                onSelectionChange={setDropdownSelectedCurrency}
+              >
+                {exchangeRateKeys.map((key) => (
+                  <Dropdown.Item key={key}>{key}</Dropdown.Item>
+                ))}
+              </Dropdown.Menu>
+            </Dropdown>
+          )}
+        </Row>
+        {inputMethod === "fiat" && (
+          <>
+            <Spacer />
+            <SatsPrice
+              exchangeRate={exchangeRates?.[watchedCurrency]}
+              fiat={watchedAmount}
+            />
+          </>
         )}
-      </Row>
-      {inputMethod === "fiat" && (
-        <>
-          <Spacer />
-          <SatsPrice exchangeRate={exchangeRates?.[currency]} fiat={amount} />
-        </>
-      )}
-      <Spacer />
-      <Button onClick={submitForm} disabled={isSubmitting || amount <= 0}>
-        {isSubmitting ? (
-          <Loading type="points" color="currentColor" size="sm" />
-        ) : (
-          <>Confirm</>
-        )}
-      </Button>
-      <Spacer />
+        <Spacer />
+        <Button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? (
+            <Loading type="points" color="currentColor" size="sm" />
+          ) : (
+            <>Confirm</>
+          )}
+        </Button>
+        <Spacer />
+      </form>
       <BackButton />
     </>
   );
