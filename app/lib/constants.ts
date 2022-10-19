dbdf9f04a7adc09e9d8ebb79e6b24792dacd70a9
Roland Bewick <roland.bewick@gmail.com>
2022-10-19 12:44:50 +0700

    Allow tips with failed withdrawal status to be reclaimed

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -4,4 +4,8 @@ export const appName = "lightsats";
 export const SATS_TO_BTC = 100000000;
 export const DEFAULT_FIAT_CURRENCY = "USD";
 
-export const refundableTipStatuses: TipStatus[] = ["UNCLAIMED", "CLAIMED"];
+export const refundableTipStatuses: TipStatus[] = [
+  "UNCLAIMED",
+  "CLAIMED",
+  "WITHDRAWAL_FAILED",
+];
