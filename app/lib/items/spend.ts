618812ffe1a14b1b0a57de0cf543008ac3ff7c30
Roland Bewick <roland.bewick@gmail.com>
2022-11-03 18:18:46 +0700

    Update btcmap link

--- app/lib/items/spend.ts
+++ app/lib/items/spend.ts
@@ -6,7 +6,7 @@ export const spend: Item[] = [
     category: "spend",
     image: "btcmap.svg",
     languageCodes: [getLanguageCode("English")],
-    link: "https://btcmap.org",
+    link: "https://lgt.st/btcmap",
     name: "BTC Map",
     slogan: "Easily find places to spend sats anywhere on the planet.",
     platforms: ["web", "mobile"],
