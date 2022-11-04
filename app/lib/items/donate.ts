9c6b37ea7ce4451b3e4385534878829740265fb8
Roland Bewick <roland.bewick@gmail.com>
2022-11-04 22:25:49 +0700

    Add tallycoin item

--- app/lib/items/donate.ts
+++ app/lib/items/donate.ts
@@ -1,3 +1,15 @@
+import { getLanguageCode } from "lib/items/getLanguageCode";
 import { Item } from "types/Item";
 
-export const donate: Item[] = [];
+export const donate: Item[] = [
+  {
+    category: "donate",
+    link: "https://tallycoin.app/",
+    image: "tallycoin.png",
+    languageCodes: [getLanguageCode("English")],
+    name: "Tallycoin",
+    slogan:
+      "Tallycoin is a bitcoin crowdfunding platform where all donations go to a wallet under your control. Zero fees.",
+    platforms: ["web"],
+  },
+];
