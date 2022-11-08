65d9499d07b32fff322eae47fc13f88a165bfc4a
Roland Bewick <roland.bewick@gmail.com>
2022-11-08 15:34:56 +0700

    Add custom phone number input with country autodetect

--- app/pages/auth/signin/phone.tsx
+++ app/pages/auth/signin/phone.tsx
@@ -1,10 +1,25 @@
-import { Button, Input, Loading, Spacer } from "@nextui-org/react";
+import {
+  Button,
+  FormElement,
+  Input,
+  Loading,
+  Row,
+  Spacer,
+  Text,
+} from "@nextui-org/react";
 import { DEFAULT_LOCALE } from "lib/i18n/locales";
 import { Routes } from "lib/Routes";
+import { defaultFetcher } from "lib/swr";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
 import React from "react";
 import { Controller, useForm } from "react-hook-form";
+import PhoneInput, {
+  Country,
+  DefaultInputComponentProps,
+} from "react-phone-number-input";
+import "react-phone-number-input/style.css";
+import useSWR from "swr";
 import { TwoFactorLoginRequest } from "types/TwoFactorLoginRequest";
 
 const formStyle: React.CSSProperties = {
@@ -24,14 +39,13 @@ type PhoneSignInProps = {
   submitText?: React.ReactNode;
 };
 
-// type TokenFormData = {
-//   token: string;
-// };
+type MyIp = { country: Country; ip: string };
 
 export default function PhoneSignIn({
   callbackUrl,
   submitText,
 }: PhoneSignInProps) {
+  const { data: myIp } = useSWR<MyIp>("https://api.country.is", defaultFetcher);
   const { t } = useTranslation("common");
   const { control, handleSubmit, setFocus } = useForm<PhoneFormData>();
   const [isSubmitting, setSubmitting] = React.useState(false);
@@ -89,21 +103,25 @@ export default function PhoneSignIn({
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)} style={formStyle}>
+        <Row>
+          <Text>{t("phone")}</Text>
+        </Row>
         <Controller
           name="phone"
           control={control}
           render={({ field }) => (
-            <Input
-              bordered
+            <PhoneInput
               {...field}
-              label={t("phone")}
-              type="tel"
-              placeholder="+66612345678"
-              autoComplete="tel"
-              fullWidth
+              placeholder="Enter phone number"
+              defaultCountry={myIp?.country}
+              inputComponent={ForwardedPhoneInput}
+              style={{
+                width: "100%",
+              }}
             />
           )}
         />
+
         <Spacer />
         <Button
           css={{ width: "100%" }}
@@ -122,3 +140,22 @@ export default function PhoneSignIn({
     </>
   );
 }
+
+const ForwardedPhoneInput = React.forwardRef<
+  unknown,
+  DefaultInputComponentProps
+>((props, ref) => {
+  const { className, ...otherProps } = props;
+  className; //  don't use the classname prop - use NextUI styling
+  return (
+    <Input
+      {...otherProps}
+      ref={ref as React.Ref<FormElement>}
+      bordered
+      aria-label="phone"
+      placeholder="0661234568"
+      fullWidth
+    />
+  );
+});
+ForwardedPhoneInput.displayName = "ForwardedInput";
