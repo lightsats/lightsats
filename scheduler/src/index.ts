c59b63b77ba9e8cc5eacee583226c01abef44e9a
Roland Bewick <roland.bewick@gmail.com>
2022-11-16 14:06:22 +0700

    Scheduler request reminders

--- scheduler/src/index.ts
+++ scheduler/src/index.ts
@@ -1,1 +1,35 @@
-console.log("Lightsats scheduler");
+require("dotenv").config();
+import fetch, { Headers } from "node-fetch";
+const appUrl = process.env.LIGHTSATS_URL;
+const appApiKey = process.env.LIGHTSATS_API_KEY;
+
+if (!appUrl || !appApiKey) {
+  throw new Error("LIGHTSATS_URL or LIGHTSATS_API_KEY unset");
+}
+
+type Reminder = {}; // fields exist, but are not required to be read by the scheduler.
+
+async function getReminders(): Promise<Reminder[]> {
+  const requestHeaders = new Headers();
+  requestHeaders.append("Accept", "application/json");
+
+  const response = await fetch(`${appUrl}/api/reminders?apiKey=${appApiKey}`, {
+    method: "GET",
+    headers: requestHeaders,
+  });
+  if (response.ok) {
+    const responseData = (await response.json()) as Reminder[];
+    return responseData;
+  } else {
+    throw new Error(
+      "Get reminders returned unexpected HTTP status: " + response.status
+    );
+  }
+}
+
+console.log("Lightsats scheduler - connecting to " + appUrl);
+
+(async () => {
+  const reminders = await getReminders();
+  console.log("Found " + reminders.length + " reminders");
+})();
