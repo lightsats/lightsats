ab8389c65d3ffc3eabf6c6b4d6251d3a3eab62fd
Roland Bewick <roland.bewick@gmail.com>
2022-11-21 20:04:56 +0700

    Update sentry config

--- app/pages/_error.tsx
+++ app/pages/_error.tsx
@@ -17,9 +17,10 @@
  */
 
 import * as Sentry from "@sentry/nextjs";
+import { NextPageContext } from "next";
 import NextErrorComponent from "next/error";
 
-const CustomErrorComponent = (props: any) => {
+const CustomErrorComponent = (props: { statusCode: number }) => {
   // If you're using a Nextjs version prior to 12.2.1, uncomment this to
   // compensate for https://github.com/vercel/next.js/issues/8592
   // Sentry.captureUnderscoreErrorException(props);
@@ -27,7 +28,7 @@ const CustomErrorComponent = (props: any) => {
   return <NextErrorComponent statusCode={props.statusCode} />;
 };
 
-CustomErrorComponent.getInitialProps = async (contextData: any) => {
+CustomErrorComponent.getInitialProps = async (contextData: NextPageContext) => {
   // In case this is running in a serverless function, await this in order to give Sentry
   // time to send the error before the lambda exits
   await Sentry.captureUnderscoreErrorException(contextData);
