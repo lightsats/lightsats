ead1db6688555266e9fb5c335c1e50534debda60
René Aaron <rene@twentyuno.net>
2022-11-22 18:17:24 +0100

    fix: navbar link

--- app/components/AppNavbar.tsx
+++ app/components/AppNavbar.tsx
@@ -55,7 +55,7 @@ export function AppNavbar() {
     () => [
       {
         name: "Home",
-        href: Routes.home,
+        href: !user ? Routes.home : Routes.dashboard,
         icon: <HomeIcon />,
       },
       {
@@ -74,7 +74,7 @@ export function AppNavbar() {
         icon: <LightBulbIcon />,
       },
     ],
-    []
+    [user]
   );
 
   if (sessionStatus === "loading" || (session && !user)) {
