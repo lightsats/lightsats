0159878b9c3daeabc22814a1125e69688b07f1b5
Dov Bitcoinheiro <50273260+bitcoinheiro@users.noreply.github.com>
2022-11-22 08:52:48 +0200

    Add ptbr to config

--- app/next-i18next.config.js
+++ app/next-i18next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "th"],
+    locales: ["en", "th", "ptbr"],
   },
   reloadOnPrerender: process.env.NEXT_PUBLIC_RELOAD_I18N === "true",
 };
