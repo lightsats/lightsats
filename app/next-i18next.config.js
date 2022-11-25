6a67677fb0678db7efd0952d3f6b53a7f51dc7d6
Roland Bewick <roland.bewick@gmail.com>
2022-11-25 12:37:26 +0700

    Update README and add simple contributing guide

--- app/next-i18next.config.js
+++ app/next-i18next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "th", "pt-BR", "de"]
+    locales: ["en", "th", "pt-BR", "de"],
   },
   reloadOnPrerender: process.env.NEXT_PUBLIC_RELOAD_I18N === "true",
 };
