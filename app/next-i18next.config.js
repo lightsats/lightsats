961def89c88d20192307dd8d7016e2021e8c142c
Roland Bewick <roland.bewick@gmail.com>
2022-12-01 11:39:45 +0700

    Add nb language language code to next config

--- app/next-i18next.config.js
+++ app/next-i18next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "th", "pt-BR", "de", "es", "fr"],
+    locales: ["en", "th", "pt-BR", "de", "es", "fr", "nb"],
   },
   reloadOnPrerender: process.env.NEXT_PUBLIC_RELOAD_I18N === "true",
 };
