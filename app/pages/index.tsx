3da53b48348b78f565f64a3adeb6edf079644da6
René Aaron <rene@twentyuno.net>
2022-12-02 11:41:44 +0100

    fix: startpage text

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -281,9 +281,12 @@ function Homepage() {
         Learn More
       </Text>
       <Text css={{ maxWidth: "500px", textAlign: "center" }}>
-        {
-          "Watch our pitch for the BOLT FUN Legends of Lightning ⚡️ tournment, Tipper and recipient flows, our team, and more"
-        }
+        This project is being built during the &nbsp;
+        <a href="https://makers.bolt.fun/tournaments/1/overview">🔩 BOLT.FUN</a>
+        &nbsp;&raquo;Legends of Lightning&laquo; tournament. Check out our&nbsp;
+        <a href="https://makers.bolt.fun/project/Lightsats">project page</a> to
+        see our pitch and to learn more about the product and the team behind
+        it.
       </Text>
       <Spacer />
       <NextLink href={Routes.about} passHref>
@@ -334,7 +337,6 @@ function TipCounter({ totalSatsSent }: { totalSatsSent: number }) {
       </Text>
       <Spacer />
       <Text h3>have been tipped to date.</Text>
-      <Text h3></Text>
     </div>
   );
 }
