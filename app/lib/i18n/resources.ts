1659bf97b950fec2ef2a0d1954e7a780f3259b77
Gabriel Silva <gabriel.silva2@ab-inbev.com>
2022-12-03 00:34:06 -0300

    feat(i18n): adding i18n for signin page

--- app/lib/i18n/resources.ts
+++ app/lib/i18n/resources.ts
@@ -1,12 +1,14 @@
 import claim from "public/locales/en/claim.json";
 import common from "public/locales/en/common.json";
 import email from "public/locales/en/email.json";
+import login from "public/locales/en/login.json";
 
 export const resources = {
   en: {
     common,
     claim,
     email,
+    login,
   },
 } as const;
 
