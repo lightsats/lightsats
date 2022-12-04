59e4cd40a38ed05def35eceb092326f442adbe5a
René Aaron <rene@twentyuno.net>
2022-12-04 22:34:10 +0100

    fix: ui tip detail

--- app/pages/tips/[id].tsx
+++ app/pages/tips/[id].tsx
@@ -189,18 +189,25 @@ const TipPage: NextPage = () => {
         {tip.status === "WITHDRAWN" && (
           <>
             <ConfettiContainer />
-            <Spacer />
-            <Text blockquote>
+            <Text
+              blockquote
+              css={{
+                background: "$primary",
+                color: "$white",
+                ta: "center",
+                fontStyle: "italic",
+              }}
+            >
               Rumors say - those who gift bitcoin are a very special kind of
               people.
             </Text>
             <Spacer />
             {placing ? (
               <>
-                <Text b>
+                <Text b css={{ ta: "center" }}>
                   {"You're"} now at&nbsp;
                   <Text
-                    color="success"
+                    color="primary"
                     b
                     size="large"
                     style={{ display: "inline" }}
@@ -210,7 +217,7 @@ const TipPage: NextPage = () => {
                   </Text>
                   &nbsp; place on the{" "}
                   <NextLink href={Routes.scoreboard} passHref>
-                    <Link style={{ display: "inline" }}>scoreboard</Link>
+                    <Link style={{ display: "inline" }}>leaderboard</Link>
                   </NextLink>
                   !
                 </Text>
