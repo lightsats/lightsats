9f5fa9f38340a3ce6a5b1d8bfc31b730886d1a27
Roland Bewick <roland.bewick@gmail.com>
2022-12-05 12:02:18 +0700

    Show small achievement icons on user card in dashboard and profile

--- app/pages/users/[id].tsx
+++ app/pages/users/[id].tsx
@@ -1,38 +1,14 @@
-import {
-  ArrowPathRoundedSquareIcon,
-  ArrowsRightLeftIcon,
-  AtSymbolIcon,
-  BoltIcon,
-  IdentificationIcon,
-  LinkIcon,
-  PlusCircleIcon,
-  UserCircleIcon,
-  UserIcon,
-  UserPlusIcon,
-  WalletIcon,
-} from "@heroicons/react/24/solid";
-import {
-  Button,
-  Card,
-  Grid,
-  Row,
-  Spacer,
-  Text,
-  Tooltip,
-} from "@nextui-org/react";
-import { AchievementType } from "@prisma/client";
-import { Icon } from "components/Icon";
+import { Button, Row, Spacer, Text } from "@nextui-org/react";
 import { NextLink } from "components/NextLink";
+import { UserAchievements } from "components/UserAchievements";
 import { UserCard } from "components/UserCard";
 import { usePublicUser } from "hooks/usePublicUser";
 import { getStaticPaths, getStaticProps } from "lib/i18n/i18next";
 import { Routes } from "lib/Routes";
 import { getUserAvatarUrl } from "lib/utils";
 import { useSession } from "next-auth/react";
-import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
 import Script from "next/script";
-import React from "react";
 
 export default function UserPublicProfile() {
   const { status: sessionStatus } = useSession();
@@ -40,46 +16,11 @@ export default function UserPublicProfile() {
   const { id } = router.query;
   const { data: publicUser } = usePublicUser(id as string, true);
 
-  const achievementStatuses = React.useMemo(
-    () =>
-      Object.values(AchievementType)
-        .map((achievementType) => ({
-          achievementType,
-          unlocked:
-            (publicUser?.achievementTypes.indexOf(achievementType) ?? -1) > -1,
-        }))
-        .filter((x) => x.unlocked)
-        .sort((a, b) => {
-          return Number(b.unlocked) - Number(a.unlocked);
-        }),
-    [publicUser?.achievementTypes]
-  );
-
   return (
     <>
       <UserCard userId={id as string} forceAnonymous />
       <Spacer />
-      <Card>
-        <Card.Header>
-          <Text h5>🏆 Achievements</Text>
-        </Card.Header>
-        <Card.Body css={{ pt: 0, pl: 0 }}>
-          <Grid.Container gap={0}>
-            {achievementStatuses.map((achievementStatus) => (
-              <Grid
-                key={achievementStatus.achievementType}
-                xs={4}
-                css={{ display: "flex", justifyContent: "center" }}
-              >
-                <AchievementBadge
-                  achievementType={achievementStatus.achievementType}
-                  unlocked={achievementStatus.unlocked}
-                />
-              </Grid>
-            ))}
-          </Grid.Container>
-        </Card.Body>
-      </Card>
+      <UserAchievements userId={id as string} showLocked />
       {publicUser?.lightningAddress && (
         <>
           <Script src="https://embed.twentyuno.net/js/app.js" />
@@ -115,168 +56,3 @@ export default function UserPublicProfile() {
 }
 
 export { getStaticProps, getStaticPaths };
-
-const achievementBadges: Record<
-  AchievementType,
-  {
-    icon: React.ReactNode;
-  }
-> = {
-  SELF_CLAIMED: {
-    icon: <ArrowPathRoundedSquareIcon />,
-  },
-  BECAME_TIPPER: {
-    icon: <UserIcon />,
-  },
-  LINKED_EMAIL: {
-    icon: <AtSymbolIcon />,
-  },
-  LINKED_WALLET: {
-    icon: <LinkIcon />,
-  },
-  SET_NAME: {
-    icon: <IdentificationIcon />,
-  },
-  SET_AVATAR_URL: {
-    icon: <UserCircleIcon />,
-  },
-  SET_LIGHTNING_ADDRESS: {
-    icon: <BoltIcon />,
-  },
-  CREATED_TIP: {
-    icon: <PlusCircleIcon />,
-  },
-  FUNDED_TIP: { icon: <WalletIcon /> },
-  TIP_CLAIMED: { icon: <UserPlusIcon /> },
-  TIP_WITHDRAWN: { icon: <ArrowsRightLeftIcon /> },
-  WEBLN_WITHDRAWN: { icon: <ArrowsRightLeftIcon /> },
-  MANUAL_WITHDRAWN: { icon: <ArrowsRightLeftIcon /> },
-  LNURL_WITHDRAWN: { icon: <ArrowsRightLeftIcon /> },
-};
-
-type AchievementBadgeProps = {
-  achievementType: AchievementType;
-  unlocked: boolean;
-};
-
-const AchievementBadge = ({
-  achievementType,
-  unlocked,
-}: AchievementBadgeProps) => {
-  const achievementBadge = achievementBadges[achievementType];
-  const { t } = useTranslation("achievements");
-
-  const styles: Record<string, React.CSSProperties> = React.useMemo(
-    () => ({
-      badge: {
-        background: `linear-gradient(to bottom right, #2E74ED 0%, #9ac0ff 100%)`,
-        filter:
-          `hue-rotate(${
-            (Object.values(AchievementType).indexOf(achievementType) /
-              Object.values(AchievementType).length) *
-            360
-          }deg)` + (!unlocked ? " grayscale(1)" : ""),
-        opacity: unlocked ? 1 : 0.75,
-        position: "relative",
-        margin: "1em 2em",
-        width: "3em",
-        height: "5em",
-        borderRadius: "6px",
-        display: "inline-block",
-        top: 0,
-        transition: "all 0.2s ease",
-      },
-      before: {
-        position: "absolute",
-        width: "inherit",
-        height: "inherit",
-        borderRadius: "inherit",
-        background: "inherit",
-        content: '""',
-        transform: "rotate(60deg)",
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        margin: "auto",
-      },
-      after: {
-        position: "absolute",
-        width: "inherit",
-        height: "inherit",
-        borderRadius: "inherit",
-        background: "inherit",
-        content: '""',
-        transform: "rotate(-60deg)",
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        margin: "auto",
-      },
-      circle: {
-        color: "#2E74ED",
-        filter: "drop-shadow(0px 4px 4px #000)",
-        width: "50px",
-        height: "50px",
-        position: "absolute",
-        paddingTop: "6px",
-        background: "#fff",
-        zIndex: 10,
-        borderRadius: "50%",
-        top: 0,
-        bottom: 0,
-        right: 0,
-        left: 0,
-        margin: "auto",
-        textAlign: "center",
-      },
-      font: {
-        display: "inline-block",
-        marginTop: "1em",
-      },
-      ribbon: {
-        position: "absolute",
-        borderRadius: "4px",
-        padding: "3px 5px",
-        textAlign: "center",
-        zIndex: 11,
-        fontSize: 11,
-        minWidth: "70px",
-        color: "#fff",
-        top: 50,
-        left: "50%",
-        transform: "translateX(-50%)",
-        boxShadow: "0 1px 2px rgba(0, 0, 0, 0.27)",
-        textShadow: "0 2px 2px rgba(0, 0, 0, 0.1)",
-        textTransform: "uppercase",
-        background: "linear-gradient(to bottom right, #555 0%, #333 100%)",
-        cursor: "default",
-      },
-    }),
-    [achievementType, unlocked]
-  );
-
-  return (
-    <Tooltip
-      content={t(`${achievementType}.description`)}
-      css={{ mt: 45 }}
-      color="primary"
-    >
-      <div style={{ width: "100px" }}>
-        <div style={styles.badge}>
-          <div style={styles.before} />
-          <div style={styles.after} />
-          <div style={styles.circle}>
-            <Icon width={32} height={32}>
-              {achievementBadge.icon}
-            </Icon>
-          </div>
-          <div style={styles.ribbon}>
-            {t(`${achievementType}.title`).replace(/[ ]+/g, "\n")}
-          </div>
-        </div>
-      </div>
-    </Tooltip>
-  );
-};
