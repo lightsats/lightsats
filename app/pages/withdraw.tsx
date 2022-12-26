510ba35e56043147e0071b8d43f76da815112320
Roland Bewick <roland.bewick@gmail.com>
2022-12-26 18:02:59 +0700

    Add placeholder to manual withdrawal input

--- app/pages/withdraw.tsx
+++ app/pages/withdraw.tsx
@@ -341,6 +341,7 @@ export function Withdraw({ flow, tipId }: WithdrawProps) {
                 <Input
                   label="Lightning Invoice"
                   fullWidth
+                  placeholder="lnbc..."
                   value={invoiceFieldValue}
                   onChange={(event) => setInvoiceFieldValue(event.target.value)}
                 />
