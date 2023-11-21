1accbcaba4a30afb65bdc84c043b037ba7c7464a
Roland Bewick <roland.bewick@gmail.com>
2023-11-21 20:41:49 +0700

    fix: remove unused migrations

--- app/prisma/migrations/20231121081837_lnbits_wallet_maintenance_mode/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "LnbitsWallet" ADD COLUMN     "maintenanceMode" BOOLEAN NOT NULL DEFAULT false;
--- app/prisma/migrations/20231121103001_remove_lnbits_wallet_maintenance_mode/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `maintenanceMode` on the `LnbitsWallet` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "LnbitsWallet" DROP COLUMN "maintenanceMode";
