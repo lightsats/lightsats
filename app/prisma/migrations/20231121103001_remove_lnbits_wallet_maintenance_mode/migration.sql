c4b9290ef5ef1a120fee0547a1e6393855614488
Roland Bewick <roland.bewick@gmail.com>
2023-11-21 17:30:17 +0700

    chore: remove unused lnbits wallet maintenance mode column

---
+++ app/prisma/migrations/20231121103001_remove_lnbits_wallet_maintenance_mode/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `maintenanceMode` on the `LnbitsWallet` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "LnbitsWallet" DROP COLUMN "maintenanceMode";
